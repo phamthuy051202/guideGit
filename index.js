82bb25c597710ff61ee9ee9d92ef3b79e41be0bc
Pham Thuy <phamthuy051202@gmail.com>
2022-07-30 08:58:39 +0700

    giua ki

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-document.querySelector("#changeTitle").onclick = () => {
-    const title = prompt("Nhập title mới");
-    document.querySelector("#container > h1").innerHTML = title;
+max = 2;
+min = 0;
+document.getElementById("button").onclick=() => {
+    var random = Math.round(Math.random() * (max - min) + min);
+    let thuy;
+    switch(random){
+        case 0:
+            thuy="Thục";
+            break;
+        case 1:
+            thuy="Thủy";
+            break;
+        case 2:
+            thuy="Trường";
+            break;
+        }
+    document.getElementsById("ab").innerHTML = thuy;
 }
+
